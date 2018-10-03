3679f48b446be10521fa30189a4b0a5f6c09c2c9
KAICHI CONG <21709414@ad.ut-capitole.fr>
2018-10-03 11:12:52 +0200

    add: change size

--- src/application/Vue.java
+++ src/application/Vue.java
@@ -15,7 +15,7 @@ public class Vue extends Button implements Observer {
 	public Vue() {
 		super();
 		this.setLabel("0");
-		this.setPreferredSize(new Dimension(120,50));
+		this.setPreferredSize(new Dimension(120,120));
 	}
 
 	@Override
